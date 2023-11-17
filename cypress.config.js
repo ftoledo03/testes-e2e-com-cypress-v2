7d4f5b1d821c1000e39dc396212bd8f6b42de46b
Fernando <jftoledoqa@gmail.com>
2023-11-17 16:43:31 -0300

    second commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseURL: 'https://notes-serverless-app.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://notes-serverless-app.com',
+    requestTimeout: 20000,
+    //setupNodeEvents(on, config) {
+    // implement node event listeners here
+    //},
   },
 })
