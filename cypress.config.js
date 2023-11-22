79791512d82cc21ca9b9c8c62c17eba07a7581fb
Fernando <jftoledoqa@gmail.com>
2023-11-22 11:45:50 -0300

    new files

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
-    requestTimeout: 20000,
+    requestTimeout: 30000,
     //setupNodeEvents(on, config) {
     // implement node event listeners here
     //},
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,88 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.visit('/signup')
+    cy.get('#email').type(email)
+    cy.get('#password').type(password, { log: false })
+    cy.get('#confirmPassword').type(password, { log: false })
+    cy.contains('button', 'Signup').click()
+    cy.get('#confirmationCode').should('be.visible')
+    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+      sentTo: email
+    }).then(message => {
+      const confirmationCode = message.html.body.match(/\d{6}/)[0]
+      cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+      cy.wait('@getNotes')
+    })
+  })
+  
+  
+Cypress.Commands.add('guiLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/login')
+  cy.get('#email').type(username)
+  cy.get('#password').type(password, { log: false })
+  cy.contains('button', 'Login').click()
+  cy.wait('@getNotes')
+  cy.contains('h1', 'Your Notes').should('be.visible')
+})
+
+Cypress.Commands.add('sessionLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  const login = () => cy.guiLogin(username, password)
+  cy.session(username, login)
+})
+
+const attachFileHandler = () => {
+  cy.get('#file').selectFile('cypress/fixtures/example.json')
+}
+
+Cypress.Commands.add('createNote', (note, attachFile = false) => {
+  cy.visit('/notes/new')
+  cy.get('#content').type(note)
+
+  if (attachFile) {
+    attachFileHandler()
+  }
+
+  cy.contains('button', 'Create').click()
+
+  cy.contains('.list-group-item', note).should('be.visible')
+})
+
+Cypress.Commands.add('editNote', (note, newNoteValue, attachFile = false) => {
+  cy.intercept('GET', '**/notes/**').as('getNote')
+
+  cy.contains('.list-group-item', note).click()
+  cy.wait('@getNote')
+
+  cy.get('#content')
+    .as('contentField')
+    .clear()
+  cy.get('@contentField')
+    .type(newNoteValue)
+
+  if (attachFile) {
+    attachFileHandler()
+  }
+
+  cy.contains('button', 'Save').click()
+
+  cy.contains('.list-group-item', newNoteValue).should('be.visible')
+  cy.contains('.list-group-item', note).should('not.exist')
+})
+
+Cypress.Commands.add('deleteNote', note => {
+  cy.contains('.list-group-item', note).click()
+  cy.contains('button', 'Delete').click()
+
+  cy.get('.list-group-item')
+    .its('length')
+    .should('be.at.least', 1)
+  cy.contains('.list-group-item', note)
+    .should('not.exist')
+})
