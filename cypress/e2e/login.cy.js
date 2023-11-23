840c8e01dbde1e267c7296476006214b8993a333
Fernando <jftoledoqa@gmail.com>
2023-11-23 10:41:42 -0300

    tentando

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,6 +3,5 @@ describe('Login', () => {
     cy.intercept('GET', '**/notes').as('getNotes')
     cy.guiLogin()
     cy.wait('@getNotes')
-    cy.contains('a', 'Create a new note').should('be.visible')
-  })
+    cy.contains('a', 'Create a new note').should('be.visible')  })
 })
