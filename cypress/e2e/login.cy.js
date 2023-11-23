33887b7fa7549c20c104dae30b68f296dafef61e
Fernando <jftoledoqa@gmail.com>
2023-11-23 10:04:38 -0300

    tentando corrigir o erro do linebreak

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,7 @@
 describe('Login', () => {
-  it('successfully logs in', () => {   
-     cy.intercept('GET', '**/notes').as('getNotes')
-
-   cy.guiLogin()
-   cy.wait('@getNotes')
-
+  it('successfully logs in', () => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.guiLogin()cy.wait('@getNotes')
     cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,21 +1,21 @@
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.visit('/signup')
-    cy.get('#email').type(email)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
-    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-      sentTo: email
-    }).then(message => {
-      const confirmationCode = message.html.body.match(/\d{6}/)[0]
-      cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-      cy.wait('@getNotes')
-    })
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+    cy.wait('@getNotes')
   })
-  
-  
+})
+
+
 Cypress.Commands.add('guiLogin', (
   username = Cypress.env('USER_EMAIL'),
   password = Cypress.env('USER_PASSWORD')
