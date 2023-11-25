4270580a7f2ddf8a785c3178c2c22ab57f94a528
Fernando <jftoledoqa@gmail.com>
2023-11-25 11:35:03 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,11 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
+    },
+
     requestTimeout:400000,
 
   },
--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -32,7 +32,7 @@ describe('Scenarios where authentication is a pre-condition', () => {
       .should('be.equal', 'Complete')
   })
 
-  it('logs out', () => {
+  it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
     cy.wait('@getNotes')
 
